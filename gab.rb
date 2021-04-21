a926173a71bcdbfaff9b0e760b435fa578890cf7
Ladislav Prskavec <ladislav@prskavec.net>
2021-04-21 07:52:18 +0200

    Brew formula update for gab version v0.3.3

--- gab.rb
+++ gab.rb
@@ -5,25 +5,25 @@
 class Gab < Formula
   desc "Github Action Badge Generator"
   homepage "https://github.com/abtris/ga-badge"
-  version "0.3.2"
+  version "0.3.3"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/abtris/ga-badge/releases/download/v0.3.2/gab_0.3.2_Darwin_x86_64.tar.gz"
-    sha256 "0d63bdf53e1612bfcb6ac6a98b1c8b683bcc1d80144fee9942cd54bd41673882"
+    url "https://github.com/abtris/ga-badge/releases/download/v0.3.3/gab_0.3.3_Darwin_x86_64.tar.gz"
+    sha256 "b35a4d02e44ebea46fd4decba477fc2dcaf6874398bf3131a635da170efc17c0"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/abtris/ga-badge/releases/download/v0.3.2/gab_0.3.2_Darwin_arm64.tar.gz"
-    sha256 "2d74c324b63d107e489a535988a6b529e43a8016fc731dc58e3e0e67d7253c6a"
+    url "https://github.com/abtris/ga-badge/releases/download/v0.3.3/gab_0.3.3_Darwin_arm64.tar.gz"
+    sha256 "6463a5ba2c38f6bc3ea82aedb87e1dfbcff5c6ee0e749340286689376af9b520"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/abtris/ga-badge/releases/download/v0.3.2/gab_0.3.2_Linux_x86_64.tar.gz"
-    sha256 "44ec2deeb83c9e4aa401820eeb741d130d5a3f592c898c579a6ba97ffad11d57"
+    url "https://github.com/abtris/ga-badge/releases/download/v0.3.3/gab_0.3.3_Linux_x86_64.tar.gz"
+    sha256 "9c4131b3560898330faaf06b9392ed7cb2d8fab35da5221cc8e992bf4aa9c447"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/abtris/ga-badge/releases/download/v0.3.2/gab_0.3.2_Linux_arm64.tar.gz"
-    sha256 "93065d1318a59e2a29e3e44ffe6e48cc57e483480a11ce5613e472340732fbd6"
+    url "https://github.com/abtris/ga-badge/releases/download/v0.3.3/gab_0.3.3_Linux_arm64.tar.gz"
+    sha256 "5393faaea403ede159ded0973327cc60b94ea23d60aa15b5ef0878420bbd545f"
   end
 
   def install
