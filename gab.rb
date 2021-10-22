18884bdda6dd9cf2e2fb1ae80ee5d56dd2484ede
Ladislav Prskavec <ladislav@prskavec.net>
2021-10-22 09:24:38 +0200

    Brew formula update for gab version v0.3.4

--- gab.rb
+++ gab.rb
@@ -5,28 +5,44 @@
 class Gab < Formula
   desc "Github Action Badge Generator"
   homepage "https://github.com/abtris/ga-badge"
-  version "0.3.3"
+  version "0.3.4"
   license "MIT"
-  bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/abtris/ga-badge/releases/download/v0.3.3/gab_0.3.3_Darwin_x86_64.tar.gz"
-    sha256 "b35a4d02e44ebea46fd4decba477fc2dcaf6874398bf3131a635da170efc17c0"
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/abtris/ga-badge/releases/download/v0.3.3/gab_0.3.3_Darwin_arm64.tar.gz"
-    sha256 "6463a5ba2c38f6bc3ea82aedb87e1dfbcff5c6ee0e749340286689376af9b520"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/abtris/ga-badge/releases/download/v0.3.3/gab_0.3.3_Linux_x86_64.tar.gz"
-    sha256 "9c4131b3560898330faaf06b9392ed7cb2d8fab35da5221cc8e992bf4aa9c447"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/abtris/ga-badge/releases/download/v0.3.3/gab_0.3.3_Linux_arm64.tar.gz"
-    sha256 "5393faaea403ede159ded0973327cc60b94ea23d60aa15b5ef0878420bbd545f"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/abtris/ga-badge/releases/download/v0.3.4/gab_0.3.4_Darwin_x86_64.tar.gz"
+      sha256 "76e650f6acdc2c76137cd48e2d4447487a6d3a00859293720e695fcfd040c2e8"
+
+      def install
+        bin.install "gab"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/abtris/ga-badge/releases/download/v0.3.4/gab_0.3.4_Darwin_arm64.tar.gz"
+      sha256 "abf3e4c9ad13ffac27ed96c19053b48ba449184c0f85891b50104f67a00dc782"
+
+      def install
+        bin.install "gab"
+      end
+    end
   end
 
-  def install
-    bin.install "gab"
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/abtris/ga-badge/releases/download/v0.3.4/gab_0.3.4_Linux_arm64.tar.gz"
+      sha256 "35205f9e41c4ae64a0f787cc8c862579f05b533e9ef67fea5a2290055eb1bbba"
+
+      def install
+        bin.install "gab"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/abtris/ga-badge/releases/download/v0.3.4/gab_0.3.4_Linux_x86_64.tar.gz"
+      sha256 "973318aaab4bd2c981d68828e563937247c4af61e20667c84a4b60fa5634b703"
+
+      def install
+        bin.install "gab"
+      end
+    end
   end
 end
